d925e96ecec242c14fdee82855f00fa75a4c3fa3
William Takes <william.e.takes@vanderbilt.edu>
2021-07-26 14:08:38 -0400

    user.js finished

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
+    database: 'furry_friends_db',
     port: 3306
 });
 
--- models/index.js
+++ models/index.js
@@ -1,7 +1,3 @@
-const router = require('express').Router();
+const User = require('./User');
 
-const userRoutes = require('./user-routes.js');
-
-router.use('/users', userRoutes);
-
-module.exports = router;
+module.exports = { User };
