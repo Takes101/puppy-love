da82845c7f4589859fd2f9c96654d890ecd33c21
William Takes <william.e.takes@vanderbilt.edu>
2021-07-26 19:37:25 -0400

    public dir completed

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-let sequelize;
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     database: 'furry_friends_db',
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(express.static('public'));
 
 
 sequelize.sync({ force: false }).then(() => {
